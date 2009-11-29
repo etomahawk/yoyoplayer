71e28c99ad64661641a619109b1f2dc15d71ab9c
hadeslee2008@gmail.com <hadeslee2008@gmail.com@2db3bad0-66f7-11de-a719-67d2479c5694>
2009-11-29 17:37:52 +0000

    重构，把上传的类单独 拿出来

--- branches/cdweb/src/java/com/chinacnd/b2b/paper/web/action/admin/product/ProductAction.java
+++ branches/cdweb/src/java/com/chinacnd/b2b/paper/web/action/admin/product/ProductAction.java
@@ -22,23 +22,14 @@ import com.chinacnd.b2b.paper.helper.json.admin.product.ProductUnitSettingJson;
 import com.chinacnd.b2b.paper.helper.json.admin.product.PulpJson;
 import com.chinacnd.b2b.paper.service.product.ProductService;
 import com.chinacnd.framework.struts.BaseAction;
-import com.chinacnd.framework.struts.ResultType;
-import com.chinacnd.framework.util.CollectionUtils;
-import com.chinacnd.framework.util.FileUtils;
-import com.chinacnd.framework.util.StringUtils;
-import com.chinacnd.framework.util.UUIDUtils;
 import com.opensymphony.xwork2.ModelDriven;
-import com.opensymphony.xwork2.inject.Inject;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Resource;
-import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.InterceptorRef;
-import org.apache.struts2.convention.annotation.Result;
 
 /**
  *
@@ -54,74 +45,6 @@ public class ProductAction extends BaseAction implements ModelDriven<ProductForm
     private List<ExtendAttributeValueForm> extendAttributeValues = new ArrayList<ExtendAttributeValueForm>();
     private List<ProductUnitSettingForm> productUnitSettings = new ArrayList<ProductUnitSettingForm>();
     private Long productId;//做为传值用
-    private File imgFile;//上传的图片文件
-    private String fileName;//上传的文件名
-    private String contentType;//内容格式
-    private Map<String, String> uploadJsonRoot;
-    private List<String> allowedImgs;//允许上传的文件后缀
-
-    @Action(value = "product-upload-image", results = {
-        @Result(type = ResultType.json, name = "upload",
-        params = {"contentType", "text/html", "root", "uploadJsonRoot"})
-    })
-    public String uploadImage() {
-        uploadJsonRoot = new HashMap<String, String>();
-        String reason = checkFile();
-        if (reason == null) {
-            String url = saveFile();
-            uploadJsonRoot.put("thumbnailUrl", url);
-            uploadJsonRoot.put("success", "true");
-            uploadJsonRoot.put("timeout", "false");
-        } else {
-            uploadJsonRoot.put("success", "false");
-            uploadJsonRoot.put("timeout", "false");
-            uploadJsonRoot.put("message", reason);
-        }
-        return "upload";
-    }
-
-    /**
-     * 把上传过来的图片文件保存起来，并返回
-     * 对应的访问URL
-     * @return 图片可访问URL
-     */
-    private String saveFile() {
-        log.info("fileName=" + fileName);
-        log.info("contentType=" + contentType);
-        String path = ServletActionContext.getServletContext().getRealPath("/");
-        String url = "/productImages/" + UUIDUtils.uuid() + "." + FileUtils.getExtention(fileName);
-        FileUtils.copy(imgFile, path + url);
-        return url;
-    }
-
-    @Inject(value = "com.chinacnd.allowed.images")
-    public void setAllowsImage(String s) {
-        allowedImgs = CollectionUtils.makeList(StringUtils.split(s, ","));
-    }
-
-    /**
-     * 检查文件是否合法
-     * 比如文件的后缀等等，因为文件格式很重要
-     * @return 错误信息，如果有的话
-     */
-    private String checkFile() {
-        if (imgFile == null) {
-            return "上传的文件为空";
-        } else {
-            String ext = FileUtils.getExtention(fileName);
-            boolean find = false;
-            for (String s : allowedImgs) {
-                if (s.equalsIgnoreCase(ext)) {
-                    find = true;
-                    break;
-                }
-            }
-            if (find == false) {
-                return "上传的文件格式非法:" + ext;
-            }
-        }
-        return null;
-    }
 
     @Action(value = "product-list-measure-units")
     public String listProductMeasureUnits() {
@@ -313,21 +236,5 @@ public class ProductAction extends BaseAction implements ModelDriven<ProductForm
 
     public PulpForm getPulp() {
         return pulp;
-    }
-
-    public void setImgFile(File imgFile) {
-        this.imgFile = imgFile;
-    }
-
-    public void setImgFileContentType(String contentType) {
-        this.contentType = contentType;
-    }
-
-    public void setImgFileFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public Map<String, String> getUploadJsonRoot() {
-        return uploadJsonRoot;
     }
 }
